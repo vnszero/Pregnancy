83f97ae82d006fa56e383885c3ef8b5645fd871d
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-05-05 19:00:17 -0300

    jetstream auth

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,13 @@ use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [RouteController::class, 'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
