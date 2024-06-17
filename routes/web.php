21d0ccf2cbb4355365cb6302b55fd20e7bf53c0d
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2024-06-17 14:32:15 -0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,12 @@
 
 use App\Http\Controllers\RouteController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', [RouteController::class, 'index']);
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
